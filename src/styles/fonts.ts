661d9f6ed910b8536a446df9a37ea0db4b4be57a
PranavMurali <pranavmurali2001@gmail.com>
2025-03-16 21:35:11 +0530

    update font

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,7 +1,7 @@
-import { Germania_One } from 'next/font/google'
-import localFont from 'next/font/local'
- 
-const sourceCodePro400 = Germania_One({ weight: '400' })
-const greatVibes = localFont({ src: '../../public/Gameplay.ttf' })
- 
-export {sourceCodePro400, greatVibes }
+import { Germania_One } from "next/font/google";
+import localFont from "next/font/local";
+
+const sourceCodePro400 = Germania_One({ weight: "400", subsets: ["latin"] });
+const greatVibes = localFont({ src: "../../public/Gameplay.ttf" });
+
+export { sourceCodePro400, greatVibes };
